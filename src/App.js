93a00dc0a67f98d50911078f104b423f72ccd93b
NguyenNgocDue <duengocnguyen@gmail.com>
2022-03-01 08:50:28 +0700

    update picture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/home/Home";
 // import Profile  from "./components/profile/Profile";
-// import Login from "./components/login/Login";
+import Login from "./components/login/Login";
 // import Register from "./components/register/Register";
 // import { Toolbar } from "@mui/material";
 
 function App() {
   return (
     <Home/>
+    // <Login/>
+
   );
 }
 
